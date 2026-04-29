3b19604480626abb0dfcc28f5667774aa7bd9ba6
jandu <jan.duettmann@icloud.com>
2026-04-29 21:02:08 +0200

    feat: Add settings design

--- app/routes/settings.py
+++ app/routes/settings.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, redirect, render_template, request, flash, url_for
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 settings_bp: Blueprint = Blueprint(name="settings", import_name=__name__)
 
 @settings_bp.route(rule="/settings", methods=["POST", "GET"])
+@login_required
 def settings():
     return render_template('settings.html')
